e99b342b76d7dd0be24fb7533c67e36e85311ed6
Kelsey-Hunt <kels.hunt13@gmail.com>
2022-10-29 21:59:41 -0500

    changed the logical expression in spinner div so that the spinner is visible if spinnerOn === true

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -37,10 +37,9 @@ STEP 4:
   Do you remember the operator we use to do "not"?
 */
 
-import React, { useState } from 'react'; /* STEP 0 */
+import React, { useState } from 'react';
 
 export default function Spinner() {
-/* STEP 1 */
 const [spinnerOn, setSpinnerOn] = useState(true);
 
   const toggleSpinner = () => {
@@ -51,7 +50,7 @@ const [spinnerOn, setSpinnerOn] = useState(true);
     <div className='widget-spinner container'>
       <h2>Spinner</h2>
       {
-        true && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
+        {spinnerOn} && <div id='spinner' className='spinner'>--+--</div>
       }
       <button id='toggleSpinner' onClick={toggleSpinner}>
         Hide Spinner {/* STEP 3 */}
