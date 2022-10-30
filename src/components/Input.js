dbfaf1d9e3e29f7361f28ff6289a38ded0e428c5
Kelsey-Hunt <kels.hunt13@gmail.com>
2022-10-29 21:02:35 -0500

    made reset set inputValue to an empty string when pushed. Updated the returned input to match inputValue (Input.js)

--- src/components/Input.js
+++ src/components/Input.js
@@ -42,11 +42,9 @@ export default function Input() {
   const changeInput = evt => {
     const { value } = evt.target;
     setInputValue(value);
-
-    /* STEP 4 */
   };
   const reset = () => {
-    /* STEP 5 */
+    setInputValue("");
   };
 
   const style = {
@@ -60,7 +58,7 @@ export default function Input() {
       <h2>Input</h2>
       <div id='output' style={style}>{inputValue.toUpperCase()}</div> 
       <div>
-        <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
+        <input id='input' type='text' onChange={changeInput} value={inputValue} /> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
       </div>
     </div>
